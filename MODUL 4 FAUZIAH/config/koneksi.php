17a1ed8958fe00a08534e05b1a13809a76745241
zulfanindar <zulfanindar@student.telkomuniversity.ac.id>
2022-11-30 08:28:25 +0700

    DEBUGGING

--- MODUL 4 FAUZIAH/config/koneksi.php
+++ MODUL 4 FAUZIAH/config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-    $koneksi = mysqli_connect("localhost", "root", "", "wad_modul4_fauziah");
+    $koneksi = mysqli_connect_error("localhost", "root", "", "wad_modul4_fauziah");
 
     if(!$koneksi){
         die("Koneksi gagal" . mysqli_connect_error());
--- MODUL 4 FAUZIAH/config/login.php
+++ MODUL 4 FAUZIAH/config/login.php
@@ -1,6 +1,6 @@
 <?php 
 
-session_start();
+session_encode();
 include("koneksi.php");
 
 if(isset($_POST['submit'])){
